78cb53530f21acc660ef76c536528288418e49c4
bigbassmatt <mkrolbass@gmail.com>
2019-04-13 13:55:57 -0400

    added led comments and macro

--- src/main.c
+++ src/main.c
@@ -4,9 +4,18 @@
 
 #include "stm32f4xx.h"
 
+/* LED      PIN
+ * green    12
+ * orange   13
+ * red      14
+ * blue     15
+ */
+
+#define LED_PIN 12
+
 void TIM2_IRQHandler(void){
-    /* toggle green led on GPIOD pin 12 */
-    GPIOD->ODR ^= (1 << 12);
+    /* toggle led */
+    GPIOD->ODR ^= (1 << LED_PIN);
     /* clear interrupt flag */
     TIM2->SR &= ~1;
 }
@@ -16,8 +25,8 @@ int main(void){
     /* enable TIM2 clock */
     RCC->APB1ENR |= RCC_APB1ENR_TIM2EN; 
 
-    /* enable GPOID pin 12 for output */
-    GPIOD->MODER |= (1 << 24);
+    /* enable led pin on GPIOD for output */
+    GPIOD->MODER |= (1 << LED_PIN*2);
 
     /* enable TIM2 interrupts first on the NVIC and then on TIM2 itself */
     NVIC->ISER[0] |= (1 << TIM2_IRQn);
